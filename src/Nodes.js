789570e4273c4474bcecc5adfbcdd4a5bca6e8dc
mooroom <61869246+mooroom@users.noreply.github.com>
2021-05-14 19:05:18 +0900

    init

---
+++ src/Nodes.js
@@ -0,0 +1,20 @@
+function Nodes({ $app, initialState }) {
+  this.state = initialState;
+
+  this.$target = document.createElement("ul");
+  $app.appendChild(this.$target);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    this.render();
+  };
+
+  this.render = () => {
+    this.$target.innerHTML = this.state.nodes.map(
+      (node) => `<li>${node.name}</li>`
+    );
+  };
+
+  this.render();
+}
